d1909ade3806acf0aeea1d73e145df492d752086
Colin Canonaco <colin.canonaco@gmail.com>
2024-11-27 20:53:10 -0500

    graph country average tones

--- avgtone.py
+++ avgtone.py
@@ -7,14 +7,20 @@ import matplotlib.ticker as ticker
 df = pd.read_csv('data/gdelt-2024-11-20-000000000000.csv')[['Actor1CountryCode', 'Actor2CountryCode', 'AvgTone']]
 
 actors = np.unique(np.concatenate((df['Actor1CountryCode'].unique(), df['Actor2CountryCode'].unique())))
-print(actors)
 
 tones = np.zeros((len(actors), len(actors)))
 for i in range(len(actors)):
   for j in range(len(actors)):
     tones[i][j] = df.loc[(df['Actor1CountryCode'] == actors[i]) & (df['Actor2CountryCode'] == actors[j])]['AvgTone'].mean()
-print(np.nanmax(tones), np.nanmin(tones))
+tones = np.nan_to_num(tones)
+avg_tones_a1 = np.mean(tones, axis=0)
+avg_tones_a2 = np.mean(tones, axis=1)
+avg_tones_all = np.sum(tones, axis=0) + np.sum(tones, axis=1)
+for i in range(len(avg_tones_all)):
+  avg_tones_all[i] -= tones[i][i]
+avg_tones_all / len(avg_tones_all)
   
+# matrix of average tone for each relationship
 tick_labels = np.insert(actors, 0, '')
 fig = plt.figure(figsize=(8,8))
 ax = fig.add_subplot()
@@ -29,4 +35,18 @@ ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
 ax.set_title('Average Tone by Relationship')
 fig.colorbar(mat, shrink=0.8)
 mat.set_clim(-4, 4)
+plt.show()
+
+# bar graph of country average tones by actor
+width = 0.35
+fig = plt.figure(figsize=(8,8))
+ax = fig.add_subplot()
+num = np.arange(len(actors))
+rectsa1 = ax.bar(num - width/2, avg_tones_a1, width, label='Actor1')
+rectsa2 = ax.bar(num + width/2, avg_tones_a2, width, label='Actor2')
+ax.set_ylabel('Average Tone')
+ax.set_title('Country Average Tone')
+ax.set_xticks(num)
+ax.set_xticklabels(actors)
+ax.legend(loc='lower right')
 plt.show()
